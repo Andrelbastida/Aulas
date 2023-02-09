cc59fa30a2246e237833f47fcefa5f779052c6c3
andrelbastida <andrelbastida@yahoo.com.br>
2023-02-09 20:07:31 -0300

    Aulas

---
+++ Aula 1/js/node.js
@@ -0,0 +1,3 @@
+var idade = Number.parseInt(window.prompt("Digite sua idade"))
+
+window.alert(idade<18?"Acesso Negado !":"Acesso Permitido !")
---
+++ Aula 2/js/node.js
@@ -0,0 +1,2 @@
+var numero = Number.parseInt(window.prompt("Digite um Número:"))
+window.alert(numero%2>0?"Este número é Impar" : "Este número é Par")
