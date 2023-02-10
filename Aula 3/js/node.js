9848e429db4b8e856ac9b17dfac0f5b50a3acc92
andrelbastida <andrelbastida@yahoo.com.br>
2023-02-09 21:14:19 -0300

    intervalo

--- Aula 3/js/node.js
+++ Aula 3/js/node.js
@@ -1,2 +1,32 @@
+/* FORMAS DE BUSCAS*/
+
+/*  - Por marca (tag HTML).
 var p = window.document.getElementsByTagName('p')[1]
 window.document.write(p.innerText)
+*/
+
+ /*- Por Id 
+var p = window.document.getElementById('p1')
+window.document.write(p.innerHTML)
+*/
+
+/* Por Seletor (Class ".")------------| ↓
+var p = window.document.querySelector('p.p1')
+window.document.write(p.innerText)
+
+Por Seletor (ID "#")------------------| ↓
+var p = window.document.querySelector('p#p1')
+window.document.write(p.innerText)
+*/
+
+    var box = window.document.getElementById("click")
+    box.addEventListener("mouseenter",entrar)
+    box.addEventListener("mouseout",sair)
+function entrar(){
+    
+    box.style.background="yellow"
+}
+function sair(){
+    
+    box.style.background="green"
+}
